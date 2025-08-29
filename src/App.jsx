7838101240f9ad5564ddd407ab1377ddb121c49f
onuigbo ifeanyi david <onuigbodavid0000@gmail.com>
2025-08-30 00:30:51 +0100

    deployment issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
+import Home from './pages/Home';
 import Login from './pages/Login.jsx';
 import SignUp from './pages/SignUp.jsx';
 
