643cec6868e9d03de4f39e68517bd21354c67918
onuigbo ifeanyi david <onuigbodavid0000@gmail.com>
2025-08-30 00:28:05 +0100

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login.jsx";
-import SignUp from "./pages/Signup.jsx";
-import Home from "./Pages/Home";
-
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import SignUp from './pages/SignUp.jsx';
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 }
